43ce8c2c0c1e4d4db8d63ce7e4a5f983fe3cec45
HaunterDaunted <61437370+HaunterDaunted@users.noreply.github.com>
2023-07-28 13:49:31 -0400

    added cooldown timer and text

---
+++ Ico Game Jam 2023/Assets/Scripts/CooldownText.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CooldownText : MonoBehaviour
+{
+	public TMP_Text text;
+	
+	private Animator thisAnimator;
+	
+	
+	private void Awake()
+	{
+		thisAnimator = GetComponent<Animator>();
+	}
+	
+	public void PlayCooldownAnimation()
+	{
+		thisAnimator.SetBool("countingDown", true);
+		Invoke("ResetCooldownAnimation", 0.6f);
+	}
+	
+	private void ResetCooldownAnimation()
+	{
+		thisAnimator.SetBool("countingDown", false);
+	}
+}
--- Ico Game Jam 2023/Assets/Scripts/GameManager.cs
+++ Ico Game Jam 2023/Assets/Scripts/GameManager.cs
@@ -4,28 +4,71 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-	private int playerBulletCooldown;
+	public int playerBulletCountdownTime;
 	
 	private PlayerController player;
+	private CooldownText cooldownText;
 	
 	
 	private void Awake()
 	{
 		player = FindObjectOfType<PlayerController>();
+		cooldownText = FindObjectOfType<CooldownText>();
 	}
 	
 	private void Start()
 	{
-		playerBulletCooldown = 3;
-		StartCoroutine(CooldownTimer());
+		playerBulletCountdownTime = 3;
+		cooldownText.text.text = playerBulletCountdownTime.ToString();
+		StartCoroutine(Countdown3());
 	}
 	
-	private IEnumerator CooldownTimer()
+	private IEnumerator Countdown3()
 	{
-		yield return new WaitForSeconds(playerBulletCooldown);
-		player.Attack();
+		cooldownText.text.text = playerBulletCountdownTime.ToString();
+		yield return new WaitForSeconds(1f);
+		DecreaseCountdownTime();
 		
-		//do this coroutine again
-		StartCoroutine(CooldownTimer());
+		StartCoroutine(Countdown2());
+	}
+	
+	private IEnumerator Countdown2()
+	{
+		cooldownText.text.text = playerBulletCountdownTime.ToString();
+		yield return new WaitForSeconds(1f);
+		DecreaseCountdownTime();
+		
+		StartCoroutine(Countdown1());
+	}
+	
+	private IEnumerator Countdown1()
+	{
+		cooldownText.text.text = playerBulletCountdownTime.ToString();
+		yield return new WaitForSeconds(1f);
+		DecreaseCountdownTime();
+		
+		StartCoroutine(Countdown0());
+	}
+	
+	private IEnumerator Countdown0()
+	{
+		cooldownText.text.text = playerBulletCountdownTime.ToString();
+		yield return new WaitForSeconds(1f);
+		DecreaseCountdownTime();
+	}
+	
+	private void DecreaseCountdownTime()
+	{
+		if (playerBulletCountdownTime > 0)
+		{
+			playerBulletCountdownTime -= 1;
+			cooldownText.PlayCooldownAnimation();
+		}
+		else
+		{
+			player.Attack();
+			playerBulletCountdownTime = 3;
+			StartCoroutine(Countdown3());
+		}
 	}
 }
