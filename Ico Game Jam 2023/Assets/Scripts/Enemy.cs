f0e32f38692773bf69d1f3ab4dbd47ed48078819
HaunterDaunted <61437370+HaunterDaunted@users.noreply.github.com>
2023-07-28 15:40:55 -0400

    Created Small Enemy

---
+++ Ico Game Jam 2023/Assets/Scripts/Enemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+	[SerializeField] private float moveSpeed;
+	
+	private GameObject target;
+	
+	
+	private void Update()
+	{
+		target = GameObject.FindGameObjectWithTag("Player");
+		if (target != null)
+		{
+			transform.position = Vector2.MoveTowards(transform.position, target.transform.position, moveSpeed * Time.deltaTime);
+		}
+		else
+		{
+			Destroy(this.gameObject);
+		}
+	}
+}
