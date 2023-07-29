d5e26d936d24a03e364ce15af3b6b2d2f06b5caf
HaunterDaunted <61437370+HaunterDaunted@users.noreply.github.com>
2023-07-29 06:29:57 -0400

    Dialogue Text
    -added dialogue text trigger prefab
    -dialogue text fades in and out when you enter and leave the trigger prefab

---
+++ Ico Game Jam 2023/Assets/Scripts/DialogueTextTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DialogueTextTrigger : MonoBehaviour
+{
+	[SerializeField] private TMP_Text dialogueTextToShow;
+	
+	
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			dialogueTextToShow.GetComponent<Animator>().SetBool("fadingIn", true);
+		}
+	}
+	
+	private void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			dialogueTextToShow.GetComponent<Animator>().SetBool("fadingIn", false);
+		}
+	}
+}
