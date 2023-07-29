0d55b2bd6603c2287e313ee49abd691b2c9727bb
HaunterDaunted <61437370+HaunterDaunted@users.noreply.github.com>
2023-07-29 08:58:10 -0400

    Added Level Exit

---
+++ Ico Game Jam 2023/Assets/Scripts/LevelExit.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelExit : MonoBehaviour
+{
+	[SerializeField] private string nextLevel;
+	[SerializeField] private Image fader;
+	[SerializeField] private GameObject playerGO;
+	
+	private PlayerMovement playerScript;
+	
+	
+	private void Awake()
+	{
+		playerScript = FindObjectOfType<PlayerMovement>();
+	}
+	
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			playerGO = other.gameObject;
+			playerGO.SetActive(false);
+			
+			StartCoroutine(FadeIntoNextLevel());
+		}
+	}
+	
+	private IEnumerator FadeIntoNextLevel()
+	{
+		fader.GetComponent<Animator>().SetBool("fadingIn", true);
+		yield return new WaitForSeconds(1f);
+		SceneManager.LoadScene(nextLevel);
+		playerGO.SetActive(true);
+	}
+}
