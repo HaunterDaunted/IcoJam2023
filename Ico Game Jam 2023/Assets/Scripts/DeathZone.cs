aabd8dd92da941ef89f8f94ebda98d4a9ab14f18
HaunterDaunted <61437370+HaunterDaunted@users.noreply.github.com>
2023-07-29 07:02:02 -0400

    Checkpoints and Respawning
    -made respawn trigger zone
    -made checkpoint for player to return to

---
+++ Ico Game Jam 2023/Assets/Scripts/DeathZone.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathZone : MonoBehaviour
+{
+	[SerializeField] private GameObject currentCheckpoint;
+	[SerializeField] private GameObject playerGO;
+	
+	private PlayerMovement playerScript;
+	
+	
+	private void Awake()
+	{
+		playerScript = FindObjectOfType<PlayerMovement>();
+	}
+	
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			playerGO = other.gameObject;
+			playerGO.SetActive(false);
+			
+			StartCoroutine(GoToLastCheckpoint());
+		}
+	}
+	
+	private IEnumerator GoToLastCheckpoint()
+	{
+		yield return new WaitForSeconds(1f);
+		playerGO.SetActive(true);
+		playerGO.transform.position = currentCheckpoint.transform.position;
+	}
+}
