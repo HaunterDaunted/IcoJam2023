71d91506d1441bd5332db35193960dc317bb2738
HaunterDaunted <61437370+HaunterDaunted@users.noreply.github.com>
2023-07-30 08:05:14 -0400

    Started on Main Menu
    -made Play Button functional

---
+++ Ico Game Jam 2023/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+	[SerializeField] private GameObject optionsMenuCanvas;
+	[SerializeField] private GameObject creditsMenuCanvas;
+	
+	
+	public void PlayGame()
+	{
+		SceneManager.LoadScene("Level1");
+	}
+}
