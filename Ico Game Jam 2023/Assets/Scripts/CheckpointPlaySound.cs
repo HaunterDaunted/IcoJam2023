3ae86edb7231522f79e24584086ba5c3778af82b
HaunterDaunted <61437370+HaunterDaunted@users.noreply.github.com>
2023-07-31 04:50:38 -0400

    Added Checkpoint SFX

---
+++ Ico Game Jam 2023/Assets/Scripts/CheckpointPlaySound.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointPlaySound : MonoBehaviour
+{
+	[SerializeField] private AudioClip checkPointSFX;
+	
+	private bool checkPointReached;
+	
+	private AudioSource thisAudioSource;
+	
+	
+	private void Awake()
+	{
+		thisAudioSource = GetComponent<AudioSource>();
+	}
+	
+	private void Start()
+	{
+		checkPointReached = false;
+	}
+	
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Player" && !checkPointReached)
+		{
+			thisAudioSource.PlayOneShot(checkPointSFX);
+			checkPointReached = true;
+		}
+	}
+}
