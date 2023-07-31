1123db620e2ceadb1c655923298e4b2edc33c884
HaunterDaunted <61437370+HaunterDaunted@users.noreply.github.com>
2023-07-31 05:48:18 -0400

    Removed Options Button because not enough time

--- Ico Game Jam 2023/Assets/Scripts/MainMenu.cs
+++ Ico Game Jam 2023/Assets/Scripts/MainMenu.cs
@@ -5,7 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-	[SerializeField] private GameObject optionsMenuCanvas;
 	[SerializeField] private AudioClip buttonClickedSFX;
 	
 	private AudioSource thisAudioSource;
